f84278e90f63e104e7fd0d27dc84975a88f4a8fb
Mark Cawthray <dr.cawthray@gmail.com>
2021-05-15 16:45:44 -0600

    Complete order test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(newOrder, deliveryOrders) {
+    if (deliveryOrders.length < 3) {
+      deliveryOrders.push(newOrder);
+  };
+  return deliveryOrders;
+};
+
+function refundOrder(orderNumToRefund, deliveryOrders) {
+for (var i = 0; i < deliveryOrders.length; i++) {
+  if (deliveryOrders[i].orderNumber === orderNumToRefund) {
+    deliveryOrders.splice([i], 1);
+  };
+};
+};
+
+function listItems(deliveryOrders) {
+  var newOrders = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    newOrders.push(deliveryOrders[i].item);
+
+  };
+  return newOrders.join(", ");
+};
+
+function searchOrder(deliveryOrders, order) {
+  var orderItems = [];
+  for (var i = 0; i < deliveryOrders.length; i++){
+    orderItems.push(deliveryOrders[i].item);
+  }
+  return orderItems.includes(order);
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
